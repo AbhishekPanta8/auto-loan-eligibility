90eb0a7a67ebe71b051e663a6da6d8cf14e31c24
Rishabh Ahuja <ahujar96@gmail.com>
2025-03-06 18:21:25 -0500

    [Commit] integrated backend api with frontend

--- backend/fastapi_app/app/main.py
+++ backend/fastapi_app/app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 from app.routes.loan import router as loan_router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(loan_router)
 
